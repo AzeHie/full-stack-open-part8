803c4327c846268596d71f16a0f55a7b2f2032e0
AzeHie <109618459+AzeHie@users.noreply.github.com>
2023-10-19 14:10:51 +0300

    Finished exercises 8.8 and 8.9

---
+++ library-frontend/src/components/Authors.js
@@ -0,0 +1,34 @@
+const Authors = ({ show, authors }) => {
+
+  if (!show) {
+    return null;
+  }
+
+  if (authors.loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.data.allAuthors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Authors;
